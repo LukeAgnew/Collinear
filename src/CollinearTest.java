38328612c376b7fe9da34074aa96bfa71fedc2d3
LukeAgnew <agnewl@ITSERV.SCSS.TCD.IE>
2016-10-12 14:45:34 +0100

    Collinear Java Program
    Java program that tests wheter three separate points in three
    dimensional space are collinear.

---
+++ src/CollinearTest.java
@@ -0,0 +1,106 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+
+//-------------------------------------------------------------------------
+/**
+*  Test class for Collinear.java
+*
+*  @author  
+*  @version 29/09/14 13:22:29
+*/
+@RunWith(JUnit4.class)
+public class CollinearTest {
+	
+	//~ Constructor ........................................................
+    @Test
+    public void testConstructor()
+    {
+      new Collinear();
+    }
+
+    //~ Public Methods ........................................................
+
+    // ----------------------------------------------------------
+    /**
+     * Check that the two methods work for empty arrays
+     */
+    @Test
+    public void testEmpty()
+    {
+        int expectedResult = 0;
+
+        assertEquals("countCollinear failed with 3 empty arrays",       expectedResult, Collinear.countCollinear(new int[0], new int[0], new int[0]));
+        assertEquals("countCollinearSorted failed with 3 empty arrays", expectedResult, Collinear.countCollinearSorted(new int[0], new int[0], new int[0]));
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Check for no false positives in a single-element array
+     */
+    @Test
+    public void testSingleFalse()
+    {
+        int[] a3 = { 15 };
+        int[] a2 = { 5 };
+        int[] a1 = { 10 };
+
+        int expectedResult = 0;
+
+        assertEquals("countCollinear({10}, {5}, {15})",       expectedResult, Collinear.countCollinear(a1, a2, a3) );
+        assertEquals("countCollinearSorted({10}, {5}, {15})", expectedResult, Collinear.countCollinearSorted(a1, a2, a3) );
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Check for no false positives in a single-element array
+     */
+    @Test
+    public void testSingleTrue()
+    {
+        int[] a3 = { 15, 5 };       int[] a2 = { 5 };       int[] a1 = { 10, 15, 5 };
+        int[] a3sorted = { 5, 15 }; int[] a2sorted = { 5 }; int[] a1sorted = { 5, 10, 15 };
+
+        int expectedResult = 1;
+
+        assertEquals("countCollinear(" + Arrays.toString(a1) + "," + Arrays.toString(a2) + "," + Arrays.toString(a3) + ")",                         expectedResult, Collinear.countCollinear(a1, a2, a3));
+        assertEquals("countCollinearSorted(" + Arrays.toString(a1sorted) + "," + Arrays.toString(a2sorted) + "," + Arrays.toString(a3sorted) + ")", expectedResult, Collinear.countCollinearSorted(a1sorted, a2sorted, a3sorted));
+    }
+
+
+    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
+    // be executed at least once from at least one test.	
+	
+	
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
